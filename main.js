c2c3955d10ef4a4f81942c1055d2cd817039e092
Lucas Bertolini <42697227+lucasbertolini@users.noreply.github.com>
2022-03-09 20:19:28 -0300

    Initial Commit

---
+++ main.js
@@ -0,0 +1,98 @@
+
+//para fazer: o botão do ponto ainda não funciona. 
+
+const calculadora = document.querySelector('#calculadora__principal');
+const exibeValor = document.querySelector('#calculadora__principal--mostrador');
+
+
+//variavéis para operação
+let valorAtual = 0;
+let valorAntigo = 0;
+var somar = false;
+var subtrair = false;
+var multiplicar = false;
+var dividir = false;
+
+calculadora.addEventListener('click', (evento) =>{
+    const valorSelecionado = evento.target.textContent;
+
+    if(evento.target.id == 'calculadora__principal--mostrador'){ //não deixa clicar na div de resultado
+        return
+    }
+
+    if(valorSelecionado == '+' || valorSelecionado == '-' || valorSelecionado == '*' || valorSelecionado == '/'){
+        //verifica se foi clicado em algum botão de operador
+        switch(valorSelecionado){
+            case "+":
+                valorAntigo = parseFloat(exibeValor.textContent);
+                exibeValor.innerHTML = '';
+                somar = true;
+                break;
+            case "-":
+                valorAntigo = parseFloat(exibeValor.textContent);
+                exibeValor.innerHTML = '';
+                subtrair = true;
+                break;
+            case "*":
+                valorAntigo = parseFloat(exibeValor.textContent);
+                exibeValor.innerHTML = '';
+                multiplicar = true;
+                break;
+            case "/":
+                valorAntigo = parseFloat(exibeValor.textContent);
+                exibeValor.innerHTML = '';
+                dividir = true;
+                break;
+        } 
+        return
+        
+    }else if(valorSelecionado == 'C'){ //para zerar o calculadora
+        valorAntigo = 0;
+        valorAtual = 0;
+        exibeValor.innerHTML = '';
+        return;
+
+    }else if(valorSelecionado == '='){ //concluir operação da calculadora
+        if(somar){
+            exibeValor.innerHTML = valorAntigo + valorAtual;
+            console.log(`valor atual ${valorAtual} \nvalor antigo ${valorAntigo} \nResultado ${exibeValor.textContent + parseInt('.')}`);
+            somar = false;
+            return
+        }else if(multiplicar){
+            exibeValor.innerHTML = valorAntigo * valorAtual;
+            multiplicar = false;
+            return 
+        }else if(dividir){
+            exibeValor.innerHTML = valorAntigo / valorAtual;
+            dividir = false;
+            return
+        }else if(subtrair){
+            exibeValor.innerHTML = valorAntigo - valorAtual;
+            subtrair = false;
+            return
+        }
+    }else if(valorSelecionado == '.'){ //adiciona o ponto
+        exibeValor.innerHTML += '.';
+        return
+
+    }
+    if(!parseInt(valorSelecionado)){ //apenas números são clicavéis
+        if(!parseInt(valorSelecionado) && parseInt(valorSelecionado) == 0 ){ //regra para o 0 funcionar corretamente
+            valorAtual = parseInt(valorSelecionado);
+            exibeValor.innerHTML += valorAtual; 
+            valorAtual = parseFloat(exibeValor.textContent)
+            return
+        }
+        console.error('Clique em um número')
+        return
+    }
+    valorAtual = parseFloat(valorSelecionado);
+    exibeValor.innerHTML += valorAtual; 
+    valorAtual = parseFloat(exibeValor.textContent);
+
+   
+   console.log(`valor atual ${valorAtual} \nvalor antigo ${valorAntigo} \nResultado ${exibeValor.textContent}`);
+    
+
+})
+
